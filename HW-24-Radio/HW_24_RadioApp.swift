9d4e66361a566edd729df90dea9003e53f838274
Ilya <iilavr@mail.ru>
2022-12-18 20:52:12 +0300

    Initial Commit

---
+++ HW-24-Radio/HW_24_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_24_RadioApp.swift
+//  HW-24-Radio
+//
+//  Created by Илья on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_24_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
